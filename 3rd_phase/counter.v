21eeedbb3b44c822cee90cd0286c3c0f1f3749bf
Cem Gulec <cem.ggulecc@gmail.com>
2019-12-15 19:56:49 +0300

    Update counter.v

--- 3rd_phase/counter.v
+++ 3rd_phase/counter.v
@@ -1,14 +1,15 @@
 module counter(
-	input aclr,clk,
+	input clr,clk,
+	input[1:0] state,
 	input[17:0] d,
-	input[1:0] func,
- 	outout reg[17:0] q
+ 	output reg[17:0] q
 );
-	always @(posedge clk, posedge aclr) 
+	always @(posedge clk, posedge clr) 
 	begin
-	  if(aclr) q <= 18'h00000;
+	  if(clr) 
+	  	q <= 18'h00000;
 	  else
-	    case(func)
+	    case(state)
 		2'b00: q<=d;
 		2'b01: q<=q+1;
 		2'b10: q<=q-1;
